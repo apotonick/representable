2ae21cc70a21f98823f74f8e5788c081f8952a48
Matthew Peychich <mpeychich@mac.com>
2013-02-17 10:36:27 -0500

    Allow the use of method names as symbols for use in conditionals

--- lib/representable.rb
+++ lib/representable.rb
@@ -28,18 +28,18 @@ require 'representable/feature/readable_writeable'
 #   hero.extend(HeroRepresenter).to_json
 module Representable
   attr_writer :representable_attrs
-  
+
   def self.included(base)
     base.class_eval do
       extend ClassInclusions, ModuleExtensions
       extend ClassMethods
       extend ClassMethods::Declarations
-      
+
       include Deprecations
       include Feature::ReadableWriteable
     end
   end
-  
+
   # Reads values from +doc+ and sets properties accordingly.
   def update_properties_from(doc, options, format)
     representable_bindings_for(format, options).each do |bin|
@@ -47,7 +47,7 @@ module Representable
     end
     self
   end
-  
+
 private
   # Compiles the document going through all properties.
   def create_representation_with(doc, options, format)
@@ -70,33 +70,35 @@ private
   # Checks and returns if the property should be included.
   def skip_property?(binding, options)
     return true if skip_excluded_property?(binding, options)  # no need for further evaluation when :exclude'ed
-    
+
     skip_conditional_property?(binding)
   end
-  
+
   def skip_excluded_property?(binding, options)
     return unless props = options[:exclude] || options[:include]
     res   = props.include?(binding.name.to_sym)
     options[:include] ? !res : res
   end
-  
+
   def skip_conditional_property?(binding)
     # TODO: move to Binding.
     return unless condition = binding.options[:if]
+
+    condition = method(condition) if condition.is_a?(Symbol)
 
     args = []
     args << binding.user_options if condition.arity > 0 # TODO: remove arity check. users should know whether they pass options or not.
 
     not instance_exec(*args, &condition)
   end
-  
+
   # Retrieve value and write fragment to the doc.
   def compile_fragment(bin, doc)
     value = get_for(bin)
-    
+
     bin.write_fragment(doc, value)
   end
-  
+
   # Parse value from doc and update the model property.
   def uncompile_fragment(bin, doc)
     bin.read_fragment(doc) do |value|
@@ -113,16 +115,16 @@ private
     value = instance_exec(value, bin.user_options, &bin.options[:setter]) if bin.options[:setter]
     send(bin.setter, value)
   end
-  
+
   def representable_attrs
     @representable_attrs ||= self.class.representable_attrs # DISCUSS: copy, or better not?
   end
-  
+
   def representable_bindings_for(format, options)
     options = cleanup_options(options)  # FIXME: make representable-options and user-options two different hashes.
     representable_attrs.map {|attr| format.build(attr, self, options) }
   end
-  
+
   # Returns the wrapper for the representation. Mostly used in XML.
   def representation_wrap
     representable_attrs.wrap_for(self.class.name)
@@ -132,14 +134,14 @@ private
   def cleanup_options(options) # TODO: remove me.
     options.reject { |k,v| [:include, :exclude].include?(k) }
   end
-  
+
   module ClassInclusions
     def included(base)
       super
       base.representable_attrs.inherit(representable_attrs)
     end
   end
-  
+
   module ModuleExtensions
     # Copies the representable_attrs to the extended object.
     def extended(object)
@@ -147,26 +149,26 @@ private
       object.representable_attrs=(representable_attrs)
     end
   end
-  
-  
+
+
   module ClassMethods
-    # Create and yield object and options. Called in .from_json and friends. 
+    # Create and yield object and options. Called in .from_json and friends.
     def create_represented(document, *args)
       new.tap do |represented|
         yield represented, *args if block_given?
       end
     end
-    
-    
+
+
     module Declarations
       def representable_attrs
         @representable_attrs ||= build_config
       end
-      
+
       def representation_wrap=(name)
         representable_attrs.wrap = name
       end
-      
+
       # Declares a represented document node, which is usually a XML tag or a JSON key.
       #
       # Examples:
@@ -181,7 +183,7 @@ private
       def property(name, options={})
         representable_attrs << definition_class.new(name, options)
       end
-      
+
       # Declares a represented document node collection.
       #
       # Examples:
@@ -193,14 +195,14 @@ private
         options[:collection] = true
         property(name, options)
       end
-      
+
       def hash(name=nil, options={})
         return super() unless name  # allow Object.hash.
-        
+
         options[:hash] = true
         property(name, options)
       end
-      
+
     private
       def definition_class
         Definition
@@ -211,19 +213,19 @@ private
       end
     end
   end
-  
-  
+
+
   # NOTE: the API of Config is subject to change so don't rely too much on this private object.
   class Config < Array
     attr_accessor :wrap
-    
+
     # Computes the wrap string or returns false.
     def wrap_for(name)
       return unless wrap
       return infer_name_for(name) if wrap === true
       wrap
     end
-    
+
     def clone
       self.class.new(collect { |d| d.clone })
     end
